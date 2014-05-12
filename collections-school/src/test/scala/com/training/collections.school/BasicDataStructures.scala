43cbf4ca941fa7d8d9cb0a9c7eac4d222cc69972
johnjunzhang <johnjunzhang@gmail.com>
2014-05-12 16:02:49 +0800

    [Jun] using should be to replace of assert

--- collections-school/src/test/scala/com/training/collections.school/BasicDataStructures.scala
+++ collections-school/src/test/scala/com/training/collections.school/BasicDataStructures.scala
@@ -8,28 +8,28 @@ import org.scalatest.{Matchers, FunSpec}
 class BasicDataStructures extends FunSpec with Matchers{
   describe("Basic Data Structures"){
     it("should get list with number 1, 2, 3, 4") {
-//      assert(list.length === 4)
-//      assert(list(0) === 1)
-//      assert(list(1) === 2)
-//      assert(list(2) === 3)
-//      assert(list(3) === 4)
+//      list.length should be(4)
+//      list(0) should be(1)
+//      list(1) should be(2)
+//      list(2) should be(3)
+//      list(3) should be(4)
     }
 
     it("should get set with number 1, 2 given init 1, 1, 2") {
-//      assert(set.size === 2)
-//      assert(set.head === 1)
-//      assert(set.last === 2)
-    }
+//      set.size should be(2)
+//      set.head should be(1)
+//      set.last should be(2)
+}
 
     it("should get tuple with two values 'localhost' and 80") {
-//      assert(tuple._1 === "localhost")
-//      assert(tuple._2 === 80)
+//      tuple._1 should be("localhost")
+//      tuple._2 should be(80)
     }
 
     it("should multiply the number by two given has value, otherwise should return 0.") {
       val map = Map("one" -> 1, "two" -> 2)
-//      assert(resultByOne === 2)
-//      assert(resultByThree === 0)
+//      resultByOne should be(2)
+//      resultByThree should be(0)
     }
   }
 }
--- collections-school/src/test/scala/com/training/collections.school/FunctionalCombinators.scala
+++ collections-school/src/test/scala/com/training/collections.school/FunctionalCombinators.scala
@@ -9,13 +9,11 @@ class FunctionalCombinators extends FunSpec with Matchers{
   describe("Functional Combinators"){
     it("should get doubledList with number 2, 4, 6, 8 using map") {
       val list = List(1, 2, 3, 4)
-      val doubledList = list.map(_ * 2)
-      doubledList.length should be(4)
-//      assert(doubledList.length === 4)
-//      assert(doubledList(0) === 2)
-//      assert(doubledList(1) === 4)
-//      assert(doubledList(2) === 6)
-//      assert(doubledList(3) === 8)
+//      doubledList.length should be (4)
+//      doubledList(0) should be (2)
+//      doubledList(1) should be (4)
+//      doubledList(2) should be (6)
+//      doubledList(3) should be (8)
     }
 
     it("should get summed result using foreach") {
@@ -25,82 +23,76 @@ class FunctionalCombinators extends FunSpec with Matchers{
       }
 
       val list = List(1, 2, 3, 4)
-//      assert(result === 10)
+//      result should be (10)
     }
 
     it("should get numbers which mod two and evaluate to 0 using filter") {
       val list = List(1, 2, 3, 4)
-//      assert(filteredList(0) == 2)
-//      assert(filteredList(1) == 4)
-    }
-
-    it("should multiply the number by two given has value, otherwise should return 0.") {
-      val map = Map("one" -> 1, "two" -> 2)
-//      assert(resultByOne === 2)
-//      assert(resultByThree === 0)
+//      filteredList(0) should be (2)
+//      filteredList(1) should be (4)
     }
 
     it("should aggregates the contents of two lists into a single list of pairs using zip") {
       val numbers = List(1, 2, 3)
       val numberWords = List("one", "two", "three")
-//      assert(aggregatedList.length === 3)
-//      assert(aggregatedList(0)._1 === 1)
-//      assert(aggregatedList(0)._2 === "one")
-//      assert(aggregatedList(1)._1 === 2)
-//      assert(aggregatedList(1)._2 === "two")
-//      assert(aggregatedList(2)._1 === 3)
-//      assert(aggregatedList(2)._2 === "three")
+//      aggregatedList.length should be (3)
+//      aggregatedList(0)._1 should be (1)
+//      aggregatedList(0)._2 should be ("one")
+//      aggregatedList(1)._1 should be (2)
+//      aggregatedList(1)._2 should be ("two")
+//      aggregatedList(2)._1 should be (3)
+//      aggregatedList(2)._2 should be ("three")
     }
 
     it("should splits lists to two lists according to whether can mod 2 and evaluate to 0 using partition") {
       val numbers = List(1, 2, 3, 4)
-//      assert(partitionedList._1(0) === 2)
-//      assert(partitionedList._1(1) === 4)
-//      assert(partitionedList._2(0) === 1)
-//      assert(partitionedList._2(1) === 3)
+//      partitionedList._1(0) should be (2)
+//      partitionedList._1(1) should be (4)
+//      partitionedList._2(0) should be (1)
+//      partitionedList._2(1) should be (3)
     }
 
     it("should get 3 given condition is greater than 2 using find") {
       val numbers = List(1, 2, 3, 4)
-//      assert(foundNumber === 3)
+//      foundNumber should be(3)
     }
 
     it("should drop 1, 2 given condition is 2 using drop") {
       val numbers = List(1, 2, 3, 4)
-//      assert(droppedNumbers(0) === 3)
-//      assert(droppedNumbers(1) === 4)
+//      droppedNumbers(0) should be (3)
+//      droppedNumbers(1) should be (4)
     }
 
-    it("should drop 1 given condition is mode 2 and evaluate not equal to 0") {
+    it("should drop 1 given condition is mode 2 and evaluate not equal to 0 using dropWhile") {
       val numbers = List(1, 2, 3, 4)
-//      assert(droppedNumbers(0) === 2)
-//      assert(droppedNumbers(1) === 3)
-//      assert(droppedNumbers(2) === 4)
+//      droppedNumbers(0) should be (2)
+//      droppedNumbers(1) should be (3)
+//      droppedNumbers(2) should be (4)
     }
 
     it("should collapses nested lists to one level using flatten") {
       val numbers = List(List(1, 2), List(3, 4))
-//      assert(flattenedNumbers.length === 4)
-//      assert(flattenedNumbers(0) === 1)
-//      assert(flattenedNumbers(1) === 2)
-//      assert(flattenedNumbers(2) === 3)
-//      assert(flattenedNumbers(3) === 4)
+//      flattenedNumbers.length should be (4)
+//      flattenedNumbers(0) should be (1)
+//      flattenedNumbers(1) should be (2)
+//      flattenedNumbers(2) should be (3)
+//      flattenedNumbers(3) should be (4)
     }
 
     it("should get doubledList with number 2, 4, 6, 8 given nested lists using flatMap") {
       val numbers = List(List(1, 2), List(3, 4))
-//      assert(flatMappedNumbers.length === 4)
-//      assert(flatMappedNumbers(0) === 2)
-//      assert(flatMappedNumbers(1) === 4)
-//      assert(flatMappedNumbers(2) === 6)
-//      assert(flatMappedNumbers(3) === 8)
+//      flatMappedNumbers.length should be (4)
+//      flatMappedNumbers(0) should be (2)
+//      flatMappedNumbers(1) should be (4)
+//      flatMappedNumbers(2) should be (6)
+//      flatMappedNumbers(3) should be (8)
     }
 
     it("should get filtered map given condition is map's value greater than 2 using filter") {
       val numberMaps = Map("one" -> 1, "two" -> 2, "three" -> 3, "four" -> 4)
-//      assert(filteredMap.size === 2)
-//      assert(filteredMap.head === ("three" -> 3))
-//      assert(filteredMap.last === ("four" -> 4))
+//      filteredMap.size should be (2)
+//      filteredMap.head should be (("three" -> 3))
+//      filteredMap.last should be (("four" -> 4))
     }
   }
 }
