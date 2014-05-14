013dc804e80ae7ccde935f23a02d882acea268a0
johnjunzhang <johnjunzhang@gmail.com>
2014-05-14 17:17:02 +0800

    [Jun] remove unused test

--- practices/src/test/scala/com/training/basics/Classes.scala
+++ practices/src/test/scala/com/training/basics/Classes.scala
@@ -17,39 +17,5 @@ class Classes extends FunSpec with Matchers{
 //      val calculator = new Calculator("HP")
 //      calculator.color should be("black")
     }
-
-    it("should get variable name given 'steve' then change to 'marius'") {
-//      name = "marius"
-//      name should be("marius")
-}
-
-    it("should get 3 given call function addOne(2)") {
-//      def addOne(number:Int, number1:Int) = number + 1
-//      val three = addOne(2, 1)
-//      three should be(3)
-    }
-
-    it("should get 3 given pass anonymous function addOne value to addOneAgain function") {
-//      def addOneAgain(preAddOne:Int => Int) = preAddOne(1) + 1
-//      val three = addOneAgain(addOne)
-//      three should be(3)
-    }
-
-    it("should get 3 given pass anonymous function addOne value to addOneAgain function using partial application") {
-//      def addOneAgain(preAddOne:Int => Int) = preAddOne(1) + 1
-//      val three = addOneAgain(addOne)
-//      three should be(3)
-    }
-
-    it("should get 6 by call multiply 2 and then 3 using curried functions") {
-//      val timesTwo = multiply(2)_
-//      val timesThree = timesTwo(3)
-//      timesThree should be(6)
-    }
-
-    it("should sum all parameters by sum functions using variable length arguments") {
-//      sum(2, 3) should be(5)
-//      sum(2, 3, 4) should be(9)
-    }
   }
 }
